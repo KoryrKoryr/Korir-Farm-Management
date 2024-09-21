ebd3f93005accc19826be59f83d6d5a28dff6395
Denis Korir <denohrkoryr@gmail.com>
2024-09-21 08:32:55 +0300

    ORM methods for Customer model

--- models/customer.py
+++ models/customer.py
@@ -2,7 +2,7 @@
 from sqlalchemy import Column, Integer, String
 
 # Import the Base class from the database module
-from database import Base
+from database import Base, session
 
 # Define a new class Customer that inherits from Base
 class Customer(Base):
@@ -12,3 +12,31 @@ class Customer(Base):
     # Define columns for the Customer table
     id = Column(Integer, primary_key=True)  # Unique identifier for each customer
     name = Column(String, nullable=False, unique=True)  # Name of the customer, cannot be null and must be unique
+
+    # Create a new customer with the given name
+    @classmethod
+    def create(cls, name):
+        customer = cls(name=name)
+        session.add(customer)
+        session.commit()
+        return customer
+    
+    # Delete a customer with given customer_id
+    @classmethod
+    def delete(cls, customer_id):
+        customer = session.query(cls).get(customer_id)
+        if customer:
+            session.delete(customer)
+            session.commit()
+        else:
+            raise ValueError(f"Customer with id {customer_id} not found!")
+        
+    # Retrieve all customers from database
+    @classmethod
+    def get_all(cls):
+        return session.query(cls).all()
+    
+    # Retrive all customers by their id from database
+    @classmethod
+    def get_by_id(cls, customer_id):
+        return session.query(cls).get(customer_id)
