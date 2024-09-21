5ec7acc5e7d2219b16ef5d21463b40b0c07c0614
Denis Korir <denohrkoryr@gmail.com>
2024-09-21 09:46:58 +0300

    Welcome message added to CLI

--- cli.py
+++ cli.py
@@ -1,4 +1,19 @@
 import click
 
+
+@click.group()
 def cli():
     pass
+
+@click.command()
+def welcome():
+    click.echo("*********************************")
+    click.echo("Welcome to Korir's Farm!")
+    click.echo("This is a simple farm management system.")
+    click.echo("Manage your farm products, orders and customers.")
+    click.echo("*********************************\n")
+
+    cli()
+
+if __name__ == '__main__':
+    welcome()
