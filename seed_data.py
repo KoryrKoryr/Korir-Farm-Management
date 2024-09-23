63fad3020b347c8d4133433e00dcc2a02ecccfbd
Denis Korir <denohrkoryr@gmail.com>
2024-09-23 20:03:38 +0300

    Seed data file created

--- seed_data.py
+++ seed_data.py
@@ -6,10 +6,13 @@ from database import session
 # Seed data for products
 def seed_products():
     products = [
-        Product(name='Maize', stock=100, price=50.75),
-        Product(name='Potatoes', stock=150, price=30.00),
-        Product(name='Avocados', stock=50, price=25.50),
-        Product(name='French Beans', stock=80, price=40.00),
+        Product(name='Beetroot', stock=100, price=50.00),
+        Product(name='Strawberries', stock=300, price=5.00),
+        Product(name='Avocados', stock=50, price=30.00),
+        Product(name='Maize', stock=400, price=800.00),
+        Product(name='Cucumber', stock=79, price=25.00),
+        Product(name='Bell Peppers', stock=138, price=15.00),
+        Product(name='Beans', stock=125, price=100.00),
     ]
     session.bulk_save_objects(products)
     session.commit()
@@ -18,9 +21,10 @@ def seed_products():
 # Seed data for customers
 def seed_customers():
     customers = [
-        Customer(name='Jane Doe'),
-        Customer(name='John Smith'),
-        Customer(name='Peter Parker'),
+        Customer(name='Cher Maeve'),
+        Customer(name='Pipo Latta'),
+        Customer(name='Lailah Chebet'),
+        Customer(name='Ian Leonard'),
     ]
     session.bulk_save_objects(customers)
     session.commit()
@@ -31,8 +35,9 @@ def seed_orders():
     # Assuming customers and products are already seeded
     orders = [
         Order(product_id=1, customer_id=1, quantity=10),
-        Order(product_id=2, customer_id=2, quantity=5),
-        Order(product_id=3, customer_id=3, quantity=12),
+        Order(product_id=2, customer_id=3, quantity=5),
+        Order(product_id=3, customer_id=2, quantity=12),
+        Order(product_id=4, customer_id=4, quantity=25),
     ]
     session.bulk_save_objects(orders)
     session.commit()
